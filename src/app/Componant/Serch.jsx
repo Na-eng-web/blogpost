a3f8523de1a0da1545f5302e17861d48a2b5f507
narendra <sonwatkar777@gmail.com>
2023-05-22 00:41:14 +0530

    new featchrue

--- src/app/Componant/Serch.jsx
+++ src/app/Componant/Serch.jsx
@@ -1,4 +1,4 @@
-import { Flex, Heading, Input, Text } from "@chakra-ui/react";
+import { Flex, Heading, Input, InputGroup, Text } from "@chakra-ui/react";
 import React from "react";
 
 const Serch = () => {
@@ -6,7 +6,8 @@ const Serch = () => {
     <Flex flexDir={"column"} align={"center"} p={4}>
       <Heading m={3}>Inside Design:Story and Interviews</Heading>
       <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</Text>
-      <Input m={5} w={"25%"} />
+
+      <Input placeholder="Serach" m={5} w={"25%"} />
     </Flex>
   );
 };
